3924add665c562764900a6b16dfde1d0c74f3fcc
Brownpaper55 <michaelawuahh@gmail.com>
2024-09-04 13:54:23 +0000

    adding quantit to my cart buttons

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,22 +1,50 @@
+from store.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
-    
-    #get the current session key
+        #Get request
+        self.request = request
+        #get the current session key if it exists
         cart = self.session.get('session_key')
 
-    #if user is new no session key create some
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
 
 
-        #make sure site works on every app
+        #make sure cart is available on all pages of the site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
+		# Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price':str(product.price)}
+            self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+
         self.session.modified = True
+
+
+    def __len__(self):
+         return len(self.cart)
+
+        
+  
+    
+    def get_prods(self):
+        if (self.cart) == None:
+            pass
+        else:
+            # Get ids from carts
+            product_ids = self.cart.keys()
+            # Use ids to look up products in database
+            products = Product.objects.filter(id__in = product_ids)
+            return products
+
+    def get_quants(self):
+        quantity = self.cart
+        return quantity
+
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -5,7 +5,10 @@ from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
-     return render(request,'summary.html')
+     cart = Cart(request)
+     cart_products = cart.get_prods
+     quantity = cart.get_quants
+     return render(request,'summary.html',{'cart_products':cart_products, 'cart_quantity':quantity})
 
 def cart_add(request):
      # Get the cart
@@ -14,11 +17,14 @@ def cart_add(request):
      if request.POST.get('action') == 'post':
           #Get stuff
           product_id = int(request.POST.get('product_id'))
+          product_qty = int(request.POST.get('product_qty'))
           # look up product in DB
           product = get_object_or_404(Product, id=product_id)
           #save to session
-          cart.add(product=product)
-          response = JsonResponse({'Product name: ': product.name})
+          cart.add(product=product, quantity= product_qty)
+          #get cart quantity
+          cart_quantity = cart.__len__()
+          response = JsonResponse({'qty': cart_quantity})
           return response
 
 
